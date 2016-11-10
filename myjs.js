350c14b99f5680f44b6d85f716bc4f3e724bf23e
Yelso <yyelso@yahoo.com>
2016-11-10 07:47:10 -0700

    First web project for hackathon
    a team project of 4 people

---
+++ myjs.js
@@ -0,0 +1,60 @@
+function mynewfunction(){
+
+alert("Survey");
+
+}
+
+function addRowTo1 Table(){
+
+var buttonnode= document.createElement('input');
+buttonnode.setAttribute('type','button');
+buttonnode.setAttribute('name','sal');
+buttonnode.setAttribute('value','sal');
+cell1.appendChild(buttonnode);
+buttonnode.attachEvent('OnClick',Hi());
+
+}
+
+function addRowTo2 Table(){
+
+var buttonnode= document.createElement('input');
+buttonnode.setAttribute('type','button');
+buttonnode.setAttribute('name','sal');
+buttonnode.setAttribute('value','sal');
+cell1.appendChild(buttonnode);
+buttonnode.attachEvent('OnClick',Hi());
+
+}
+
+function addRowTo3 Table(){
+
+var buttonnode= document.createElement('input');
+buttonnode.setAttribute('type','button');
+buttonnode.setAttribute('name','sal');
+buttonnode.setAttribute('value','sal');
+cell1.appendChild(buttonnode);
+buttonnode.attachEvent('OnClick',Hi());
+
+}
+
+function addRowTo4 Table(){
+
+var buttonnode= document.createElement('input');
+buttonnode.setAttribute('type','button');
+buttonnode.setAttribute('name','sal');
+buttonnode.setAttribute('value','sal');
+cell1.appendChild(buttonnode);
+buttonnode.attachEvent('OnClick',Hi());
+
+}
+
+function addRowTo5 Table(){
+
+var buttonnode= document.createElement('input');
+buttonnode.setAttribute('type','button');
+buttonnode.setAttribute('name','sal');
+buttonnode.setAttribute('value','sal');
+cell1.appendChild(buttonnode);
+buttonnode.attachEvent('OnClick',Hi());
+
+}
